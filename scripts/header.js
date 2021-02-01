ff6c34f6c21e86a3dbb6570a3bc2942f89a9ac54
Фомин Андрей Александрович <fomin_a@tochka.com>
2021-02-01 23:04:12 +0500

    autoplay

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,10 @@
-var video = document.getElementById("header__video");
-var delayMillis = 4000;
-video.volume = 0;
+(function() {
+    var video = document.getElementById("header__video");
+    var delayMillis = 1000;
+    video.volume = 0;
 
-setTimeout(function() {
-    video.play();
-}, delayMillis);
+    setTimeout(function() {
+        video.play();
+    }, delayMillis);
+})()
 
